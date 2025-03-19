e8e087af39109dcdc98327b4c7021b098d620a03
unknown <dcorehost3@gmail.com>
2025-03-19 10:39:02 +0530

    take fresh pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,111 @@
+
+
+
+// import React from 'react';
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar';
+// import SideBar from './Components/AdminSideBar/AdminSideBar';
+// import Signup from './Components/Signup/Signup';
+// import SignIn from './Components/SignIn/SignIn';
+// import Logout from './Components/Logout/Logout';
+// import AdminDashboard from './Pages/AdminDashboard/AdminDashboard';
+// import UserProfile from './Components/UseProfile/UserProfile';
+// import ResetPassword from './Components/ResetPassword/ResetPassword';
+// import UserManagement from './Components/UserManagement/UserManagement';
+// import SalesManagement from './Components/SalesManagement/Salesmanagement';
+// import EnquiryManagement from './Components/EnquiryManagement/EnquiryManagement';
+// import TaskManager from './Components/TaskManager/TaskManager';
+// import EnquiryDetails from './Components/EnquiryDetails/EnquiryDetails';
+// import PurchaseManagement from './Components/PurchaseManagement/PurchaseManagement';
+// import PurchaseReport from './Components/PurchaseReport/PurchaseReport';
+// import SalesDetails from './Components/SalesDetails/SalesDetails';
+// import LeaveManagement from './Components/LeaveManagement/LeaveManagement';
+// import UserSidebar from './Components/UserSidebar/UserSidebar';
+// import SuperAdminSidebar from './Components/SuperAdminSidebar/SuperAdminsidebar';
+// import LeaveSummary from './Components/LeaveSummary/LeaveSummary';
+
+
+
+
+// const App = () => {
+//   const token = localStorage.getItem("token");
+//   const typeOfUser = localStorage.getItem("typeOfUser");
+
+//   return (
+//     <Router>
+//       {!token ? (
+//         <>
+//           <Navbar />
+//           <Routes>
+//             <Route path="/" element={<SignIn />} />
+//             {/* <Route path="/signup" element={<Signup />} /> */}
+//             <Route path="*" element={<Navigate to="/" />} />
+//           </Routes>
+//         </>
+//       ) : (
+//         <>
+//           {/* Show Sidebar based on user type */}
+//           <Routes>
+//           {/* {typeOfUser === "superadmin" && <Route path="*" element={<Navigate to="/superadmin-sidebar" />} />} */}
+//             {typeOfUser === "Admin" && <Route path="*" element={<Navigate to="/admin-sidebar" />} />}
+//             {/* {typeOfUser === "User" && <Route path="*" element={<Navigate to="/user-sidebar" />} />} */}
+//           </Routes>
+
+//           {/* 🔹 Sidebar Components */}
+//           <Routes>
+//             <Route path="/admin-sidebar" element={<SideBar />} />
+//             <Route path="/user-sidebar" element={<UserSidebar />} />
+//           {/* </Routes>
+
+//           <Routes> */}
+//             <Route path="/admin-dashboard" element={<AdminDashboard />} />
+//             <Route path="/resetpassword" element={<ResetPassword />} />
+//             <Route path="/logout" element={<Logout />} />
+//             <Route path="/userProfile" element={<UserProfile />} />
+//             <Route path="/sales-management" element={<SalesManagement />} />
+//             <Route path="/sales-report" element={<SalesDetails />} />
+//             <Route path="/enquiry-management" element={<EnquiryManagement />} />
+//             <Route path="/enquiries-details" element={<EnquiryDetails />} />
+//             <Route path="/purchase-management" element={<PurchaseManagement />} />
+//             <Route path="/purchase-report" element={<PurchaseReport />} />
+//             <Route path="/leave-management" element={<LeaveManagement />} />
+//             <Route path="/leave-summary" element={<LeaveSummary />} />
+          
+//           </Routes>
+
+//           {typeOfUser === "admin" && ( <SideBar>
+//             <Routes>
+//             <Route path="/user-management" element={<UserManagement />} />
+//             <Route path="/task-manager" element={<TaskManager />} />
+//             </Routes>
+//             </SideBar>
+//           )}
+
+//           {typeOfUser === "user" && ( <UserSidebar>
+//             <Routes>
+//             <Route path="/leave-summary" element={<LeaveSummary />} />
+//             <Route path="/leave-management" element={<LeaveManagement/>} />
+//             </Routes>
+//             </UserSidebar>
+//           )}
+
+//           {typeOfUser === "superadmin" && ( <SuperAdminSidebar>
+//             <Routes>
+//             <Route path="/user-management" element={<UserManagement />} />
+//             <Route path="/task-manager" element={<TaskManager />} />
+//             </Routes>
+//             </SuperAdminSidebar>
+//           )}
+//         </>
+//       )}
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -22,9 +130,6 @@ import UserSidebar from './Components/UserSidebar/UserSidebar';
 import SuperAdminSidebar from './Components/SuperAdminSidebar/SuperAdminsidebar';
 import LeaveSummary from './Components/LeaveSummary/LeaveSummary';
 
-
-
-
 const App = () => {
   const token = localStorage.getItem("token");
   const typeOfUser = localStorage.getItem("typeOfUser");
@@ -32,46 +137,78 @@ const App = () => {
   return (
     <Router>
       {!token ? (
+        // Routes for unauthenticated users
         <>
           <Navbar />
           <Routes>
             <Route path="/" element={<SignIn />} />
-            {/* <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </>
       ) : (
+        // Routes for authenticated users
         <>
-          {/* Show Sidebar based on user type */}
-          <Routes>
-          {typeOfUser === "superadmin" && <Route path="*" element={<Navigate to="/superadmin-sidebar" />} />}
-            {typeOfUser === "Admin" && <Route path="*" element={<Navigate to="/admin-sidebar" />} />}
-            {typeOfUser === "User" && <Route path="*" element={<Navigate to="/user-sidebar" />} />}
-          </Routes>
+          {/* Render Sidebar based on user type */}
+          {typeOfUser === "Admin" && <SideBar />}
+          {typeOfUser === "User" && <UserSidebar />}
+          {typeOfUser === "superadmin" && <SuperAdminSidebar />}
 
-          {/* 🔹 Sidebar Components */}
+          {/* Main content routes */}
           <Routes>
-            <Route path="/admin-sidebar" element={<SideBar />} />
-            <Route path="/user-sidebar" element={<UserSidebar />} />
-            <Route path="/superadmin-sidebar" element={<SuperAdminSidebar />} />
-          {/* </Routes>
-
-          <Routes> */}
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-            <Route path="/user-management" element={<UserManagement />} />
+            {/* Common routes for all authenticated users */}
             <Route path="/resetpassword" element={<ResetPassword />} />
             <Route path="/logout" element={<Logout />} />
-            <Route path="/us  erProfile" element={<UserProfile />} />
-            <Route path="/sales-management" element={<SalesManagement />} />
-            <Route path="/sales-report" element={<SalesDetails />} />
-            <Route path="/enquiry-management" element={<EnquiryManagement />} />
-            <Route path="/task-manager" element={<TaskManager />} />
-            <Route path="/enquiries-details" element={<EnquiryDetails />} />
-            <Route path="/purchase-management" element={<PurchaseManagement />} />
-            <Route path="/purchase-report" element={<PurchaseReport />} />
-            <Route path="/leave-management" element={<LeaveManagement />} />
-            <Route path="/leave-summary" element={<LeaveSummary />} />
-          
+            <Route path="/userProfile" element={<UserProfile />} />
+
+            {/* Admin-specific routes */}
+            {typeOfUser === "Admin" && (
+              <>
+                <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                <Route path="/user-management" element={<UserManagement />} />
+                <Route path="/task-manager" element={<TaskManager />} />
+                <Route path="/sales-management" element={<SalesManagement />} />
+                <Route path="/sales-report" element={<SalesDetails />} />
+                <Route path="/enquiry-management" element={<EnquiryManagement />} />
+                <Route path="/enquiries-details" element={<EnquiryDetails />} />
+                <Route path="/purchase-management" element={<PurchaseManagement />} />
+                <Route path="/purchase-report" element={<PurchaseReport />} />
+                <Route path="/leave-management" element={<LeaveManagement />} />
+                <Route path="/leave-summary" element={<LeaveSummary />} />
+              </>
+            )}
+
+            {/* User-specific routes */}
+            {typeOfUser === "User" && (
+              <>
+                <Route path="/leave-summary" element={<LeaveSummary />} />
+                <Route path="/leave-management" element={<LeaveManagement />} />
+              </>
+            )}
+
+            {/* Superadmin-specific routes */}
+            {typeOfUser === "superadmin" && (
+              <>
+                <Route path="/user-management" element={<UserManagement />} />
+                <Route path="/task-manager" element={<TaskManager />} />
+              </>
+            )}
+
+            {/* Redirect to appropriate dashboard based on user type */}
+            <Route
+              path="*"
+              element={
+                typeOfUser === "Admin" ? (
+                  <Navigate to="/admin-dashboard" />
+                ) : typeOfUser === "User" ? (
+                  <Navigate to="/leave-summary" />
+                ) : typeOfUser === "superadmin" ? (
+                  <Navigate to="/user-management" />
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
           </Routes>
         </>
       )}
@@ -80,5 +217,3 @@ const App = () => {
 };
 
 export default App;
-
-
--- src/Components/UserSidebar/UserSidebar.jsx
+++ src/Components/UserSidebar/UserSidebar.jsx
@@ -1,94 +1,242 @@
 
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaChartLine, FaChalkboardTeacher, FaChevronUp, FaChevronDown } from "react-icons/fa";
+// import { MdPlayLesson } from "react-icons/md";
+// import { RiAdminFill, RiTeamFill } from "react-icons/ri";
+// import { LiaBuysellads } from "react-icons/lia";
+// import styles from "./SuperAdminSidebar.module.css";
+// import Navbar from "../Navbar/Navbar";
+// import { FaBuildingUser } from "react-icons/fa6";
+// import { GrTasks } from "react-icons/gr";
+// import { FaUser } from "react-icons/fa";
+// import { FcSalesPerformance } from "react-icons/fc";
+// import { BiSolidCoinStack } from "react-icons/bi";
+// import { BiSolidMessageSquareDots } from "react-icons/bi";
+
+// const SuperAdminSidebar = ({ children }) => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [activeMenu, setActiveMenu] = useState(null);
+
+//   const menus = [
+//     {
+//       title: "Dashboard",
+//       icon: <RiAdminFill />,
+//       link: "/#",
+//       submenus: [
+//         { title: "User Management", icon: <FaBuildingUser />, link: "/user-management" },
+//         { title: "Task Manager", icon: <GrTasks />, link: "/task-manager" },
+//         { title: "Enquiry Table", icon: <BiSolidMessageSquareDots />, link: "/enquiries-details" },
+//         { title: "User Profile", icon: <FaUser />, link: "/userProfile" },
+//         { title: "Sales Report", icon: <FcSalesPerformance />, link: "/sales-report" },
+//         { title: "Purchase Report", icon: <BiSolidCoinStack />, link: "/purchase-report" },
+//         { title: "Option", icon: <FaBuildingUser />, link: "/sign" },
+//       ],
+//     },
+//     {
+//       title: "Admin Management",
+//       icon: <RiTeamFill />,
+//       link: "/#",
+//       submenus: [
+//         { title: "User Profile",    icon: <FaUser />         , link: "/userProfile" },
+//         { title: "Option 1",        icon: <FaBuildingUser /> , link: "/option-1" },
+//         { title: "Option 2",        icon: <FaBuildingUser /> , link: "/option-2" },
+//         { title: "Option 3",        icon: <FaBuildingUser /> , link: "/option-3" },
+//       ],
+//     },
+//     {
+//       title: "Role & Permission",
+//       icon: <LiaBuysellads />,
+//       link: "/#",
+//       submenus: [
+//         { title: "Generate Sales",   icon: <FcSalesPerformance /> ,         link: "/sales-management" },
+//         { title: "Generate Purchase ", icon: <BiSolidCoinStack /> ,         link: "/purchase-management" },
+//         { title: "User Profile",     icon: <FaUser />         ,             link: "/userProfile" },
+//         { title: "Option 3",         icon: <FaBuildingUser /> ,             link: "/option-3" },
+//       ],
+//     },
+    
+//     {
+//       title: "System Setting",
+//       icon: <FaChartLine />,
+//       link: "/#",
+//       submenus: [
+//         { title: "Generate Purchase ", icon: <FaBuildingUser /> , link: "/purchase-management" },
+//         { title: "Option 2",           icon: <FaBuildingUser /> , link: "/option-2" },
+//         { title: "Option 3",           icon: <FaBuildingUser /> , link: "/option-3" },
+//       ],
+//     },
+//     {
+//       title: "Security & packing",
+//       icon: <FaChalkboardTeacher />,
+//       link: "/#",
+//       submenus: [
+//         { title: "User Profile",     icon: <FaUser />         ,           link: "/userProfile" },
+//         { title: "Leave Management",       icon: <FaBuildingUser /> ,             link: "/leave-management" },
+//         { title: "Option 3",       icon: <FaBuildingUser /> ,             link: "/option-3" },
+//       ],
+//     },
+//     {
+//       title: "Subscription  & Billing",
+//       icon: <MdPlayLesson />,
+//       link: "/#",
+//       submenus: [
+//         { title: "Option 1",   icon: <FaBuildingUser /> , link: "/option-1" },
+//         { title: "Option 2",   icon: <FaBuildingUser /> , link: "/option-2" },
+//         { title: "Option 3",   icon: <FaBuildingUser /> , link: "/option-3" },
+//       ],
+//     },
+//     {
+//       title: "Reports & Analytics",
+//       icon: <FaChartLine />,
+//       link: "/#",
+//       submenus: [
+//         { title: "Option 1",   icon: <FaBuildingUser /> , link: "/option-1" },
+//         { title: "Option 2",   icon: <FaBuildingUser /> , link: "/option-2" },
+//         { title: "Option 3",   icon: <FaBuildingUser /> , link: "/option-3" },
+//       ],
+//     },
+//     // Additional menus here
+//   ];
+
+//   const toggleSidebar = () => setIsOpen(!isOpen);
+//   const toggleSubmenu = (index) => setActiveMenu(activeMenu === index ? null : index);
+
+//   return (
+//     <div className={`${styles.sidebarWrapper}`}>
+//       <div className={`${styles.sidebarContainer}`}>
+//         <button
+//           className={styles.hamburgerButton}
+//           onClick={toggleSidebar}
+//           aria-expanded={isOpen}
+//           aria-label="Toggle sidebar"
+//         >
+//           ☰
+//         </button>
+
+//         <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+//           <ul className={styles.menu}>
+//             {menus.map((menu, index) => (
+//               <React.Fragment key={index}>
+//                 <li>
+//                   <div
+//                     className={styles.menuItem}
+//                     onClick={() => toggleSubmenu(index)}
+//                   >
+//                     <div className={styles.icon}>{menu.icon}</div>
+//                       <span className={`${styles.title} ${!isOpen ? styles.hidden : ""}`} >
+//                         {menu.title}
+//                      </span>
+//                     {menu.submenus.length > 0 && (
+//                       <span className={styles.dropdownIcon}>
+//                         {activeMenu === index ? <FaChevronUp /> : <FaChevronDown />}
+//                       </span>
+//                     )}
+//                   </div>
+//                   {activeMenu === index && (
+//                     <ul className={styles.submenu}>
+//                       {menu.submenus.map((submenu, subIndex) => (
+//                         <li key={subIndex} className={styles.submenuItem}>
+//                           <div className={styles.submenuItemWrapper}>
+//                             <span className={styles.icon}>{submenu.icon}</span>
+//                             <Link to={submenu.link} className={styles.submenuLink}>
+//                               {submenu.title}
+//                             </Link>
+//                           </div>
+//                         </li>
+//                       ))}
+//                     </ul>
+//                   )}
+//                 </li>
+//                 <hr className={styles.menuDivider} />
+//               </React.Fragment>
+//             ))}
+//             <li>
+//               <Link to="/logout" className={`${styles.menuItem} ${styles.logout}`}>
+//                 <FaChartLine />
+//                 <span
+//                   className={`${styles.title} ${!isOpen ? styles.hidden : ""}`}
+//                 >
+//                   Logout
+//                 </span>
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//       <div className={styles.contentMenu}>
+//         <Navbar isOpen={isOpen} />
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SuperAdminSidebar;
+
+
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaChartLine, FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { RiAdminFill, RiTeamFill } from "react-icons/ri";
 import styles from "./UserSidebar.module.css";
 import Navbar from "../Navbar/Navbar";
+import { FaBuildingUser } from "react-icons/fa6";
+import { GrTasks } from "react-icons/gr";
+import { FaUser } from "react-icons/fa";
+import { FcSalesPerformance } from "react-icons/fc";
+import { BiSolidCoinStack, BiSolidMessageSquareDots } from "react-icons/bi";
 
 const UserSidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeMenu, setActiveMenu] = useState(null);
-  const [activeSubmenu, setActiveSubmenu] = useState(null);
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const navigate = useNavigate();
 
   const menus = [
     {
       title: "Dashboard",
-      icon: <RiAdminFill />,
+      icon: <FaChartLine />, 
       link: "/#",
       submenus: [
-        { title: "My Space", link: "##", hasNested: true },
-        { title: "Organization", link: "##", hasNested: true },
-      ],
-    },
-    {
-      title: "Leave Summary",
-      icon: <RiTeamFill />,
-      link: "/#",
-      submenus: [
-        { title: "My Data", link: "###", hasNested: true },
-        { title: "Team", link: "##", hasNested: true },
-        { title: "Holidays", link: "##", hasNested: true },
+        { title: "Leave Summary", icon: <FaBuildingUser />, link: "/leave-summary" },
+        { title: "Leave Management", icon: <GrTasks />, link: "/leave-management" },
       ],
     },
   ];
-
-  const mySpaceSubmenus = [
-    { title: "Overview", link: "#" },
-    { title: "Dashboard", link: "#" },
-  ];
-
-  const organizationSubmenus = [
-    { title: "Overview", link: "#" },
-    { title: "Announcements", link: "#" },
-    { title: "Policies", link: "#" },
-    { title: "Employee Tree", link: "#" },
-    { title: "Birthday Folks", link: "#" },
-  ];
-  const myDataSubmenus = [
-    
-    { title: "Leave Summary", link: "/leave-summary"},
-    { title: "Leave Balance", link: "#" },
-    { title: "Leave Requests", link: "#" },
-  ];
-  console.log("data",myDataSubmenus)
-
 
   const toggleSidebar = () => setIsOpen(!isOpen);
-  const toggleSubmenu = (index) => {
-    setActiveMenu(activeMenu === index ? null : index);
-    setActiveSubmenu(null);
-  };
-  const handleSubmenuClick = (subIndex) => setActiveSubmenu(subIndex);
+  const toggleSubmenu = (index) => setActiveMenu(activeMenu === index ? null : index);
 
   const handleLogout = () => {
     console.log("Logout clicked! Clearing localStorage...");
     localStorage.clear();
     sessionStorage.clear();
-    window.location.href = "/"; // ✅ Instant Logout Redirect
+    setIsLoggedOut(true);
+    window.location.href = "/";
   };
 
-  return (
-    <div className={styles.sidebarWrapper}>
-      <div className={styles.sidebarContainer}>
-        <button
-          className={styles.hamburgerButton}
-          onClick={toggleSidebar}
-          aria-expanded={isOpen}
-          aria-label="Toggle sidebar"
-        >
-          ☰
-        </button>
+  if (isLoggedOut) {
+    navigate("/");
+  }
 
-        <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+  return (
+    <div className={`${styles.sidebarWrapper}`}>
+      <div className={`${styles.sidebarContainer}`}>
+        {/* <button className={styles.hamburgerButton} onClick={toggleSidebar}>
+          ☰
+        </button> */}
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+          {/* ☰ (Open) / ✖ (Close) Button */}
+          <button className={styles.hamburgerButton} onClick={toggleSidebar}>
+            {isOpen ? "✖" : "☰"}
+          </button>
+        {/* <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}> */}
           <ul className={styles.menu}>
             {menus.map((menu, index) => (
               <React.Fragment key={index}>
                 <li>
-                  <div
-                    className={styles.menuItem}
-                    onClick={() => toggleSubmenu(index)}
-                  >
+                  <div className={styles.menuItem} onClick={() => toggleSubmenu(index)}>
                     <div className={styles.icon}>{menu.icon}</div>
                     <span className={`${styles.title} ${!isOpen ? styles.hidden : ""}`}>
                       {menu.title}
@@ -103,42 +251,12 @@ const UserSidebar = ({ children }) => {
                     <ul className={styles.submenu}>
                       {menu.submenus.map((submenu, subIndex) => (
                         <li key={subIndex} className={styles.submenuItem}>
-                          <button
-                            className={styles.submenuButton}
-                            onClick={() => handleSubmenuClick(subIndex)}
-                          >
-                            {submenu.title}
-                          </button>
-
-                          {activeSubmenu === subIndex && submenu.title === "My Space" && (
-                            <div className={styles.nestedSubmenuContainer}>
-                              {mySpaceSubmenus.map((item, idx) => (
-                                <button key={idx} className={styles.submenuButton}>
-                                  {item.title}
-                                </button>
-                              ))}
-                            </div>
-                          )}
-
-                          {activeSubmenu === subIndex && submenu.title === "Organization" && (
-                            <div className={styles.nestedSubmenuContainer}>
-                              {organizationSubmenus.map((item, idx) => (
-                                <button key={idx} className={styles.submenuButton}>
-                                  {item.title}
-                                </button>
-                              ))}
-                            </div>
-                          )}
-
-                          {activeSubmenu === subIndex && submenu.title === "My Data" && (
-                            <div className={styles.nestedSubmenuContainer}>
-                              {myDataSubmenus.map((item, idx) => (
-                                <button key={idx} className={styles.submenuButton}>
-                                  {item.title}
-                                </button>
-                              ))}
-                            </div>
-                          )}
+                          <div className={styles.submenuItemWrapper}>
+                            <span className={styles.icon}>{submenu.icon}</span>
+                            <Link to={submenu.link} className={styles.submenuLink}>
+                              {submenu.title}
+                            </Link>
+                          </div>
                         </li>
                       ))}
                     </ul>
@@ -147,14 +265,10 @@ const UserSidebar = ({ children }) => {
                 <hr className={styles.menuDivider} />
               </React.Fragment>
             ))}
-
-            {/* Logout Button */}
-            <li className={styles.logoutContainer}>
-              <button onClick={handleLogout} className={styles.logout}>
+            <li>
+              <button onClick={handleLogout} className={`${styles.menuItem} ${styles.logout}`}>
                 <FaChartLine />
-                <span className={`${styles.title} ${!isOpen ? styles.hidden : ""}`}>
-                  Logout
-                </span>
+                <span className={`${styles.title} ${!isOpen ? styles.hidden : ""}`}>Logout</span>
               </button>
             </li>
           </ul>
@@ -169,3 +283,6 @@ const UserSidebar = ({ children }) => {
 };
 
 export default UserSidebar;
+
+
+
