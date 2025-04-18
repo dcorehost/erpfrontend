5d948d201f5abc6f7fbb06c224b3063601713594
Deepika <dcorehost2@gmail.com>
2025-04-18 11:19:06 +0530

    Track CSS file

--- src/Pages/UserDashboard/DashboardUser.jsx
+++ src/Pages/UserDashboard/DashboardUser.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./UserDashboard.module.css";
+import styles from "./DashboardUser.module.css";
 import { motion } from "framer-motion";
 import axios from "axios";
 import { toast } from "react-toastify";
